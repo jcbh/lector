a6033bf9c31cbacae035ae69432b85919714c88b
Juan Carlos Ballesteros Hermida <jcballesteroshermida@gmail.com>
2019-10-06 18:43:08 +0200

    Multidioma

--- src/main/java/org/tecnificados/com/lector/App.java
+++ src/main/java/org/tecnificados/com/lector/App.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
@@ -18,48 +19,57 @@ import org.slf4j.LoggerFactory;
  */
 public class App 
 {
+	private static final String UTF_8 = "utf-8";
+
+	private static final String CONF_PROPERTIES = "conf.properties"; 
+
+	private static final String PATH_TO_FILE = "pathToFile"; 
+
 	private static final Logger log = LoggerFactory.getLogger(App.class);
 	
-	private static String filePath="fichero.txt";
+	private static String filePath="fichero.txt"; 
 	
 	private static void configuration() {
 		Properties prop = new Properties();
     	try 
     	{
-    		InputStream input = new FileInputStream("conf.properties");
+    		InputStream input = new FileInputStream(CONF_PROPERTIES);
     		prop.load(input);    	         	    
     	} 
     	catch (IOException ex) {
-    	    log.error("Error reading  configuration file",ex);
+    	    log.error(Messages.getString("App.3"),ex); 
     	}
     	
-    	if (prop.getProperty("pathToFile")!=null) {
-    		filePath=prop.getProperty("pathToFile");
+    	if (prop.getProperty(PATH_TO_FILE)!=null) {
+    		filePath=prop.getProperty(PATH_TO_FILE);
     	}else {
-    		log.info("pathToFile not readed in conf.properties");
+    		log.info(PATH_TO_FILE+Messages.getString("App.4")+CONF_PROPERTIES); 
     	}
 		
 	}
 	
     public static void main( String[] args )
-    {
-    	log.info("Inicio");
+    {    	
+    	Locale.setDefault(new Locale("es_ES"));
+    
+    	
+    	log.info(Messages.getString("App.5")); 
     	    	  	
     	configuration();    	
     	
     	List<String> readedLines = new ArrayList<String>();
         try {
-        	readedLines = FileUtils.readLines(new File(filePath),"utf-8");
-        	log.info("Lineas: "+readedLines.size());
+        	readedLines = FileUtils.readLines(new File(filePath),UTF_8);
+        	log.info(Messages.getString("App.7")+readedLines.size()); 
 			
 		} catch (IOException e) {
-			log.error("Error reading file",e);
+			log.error(Messages.getString("App.8"),e); 
 		}
         for (String actual:readedLines)
         {
 			log.info(actual);
         }
-        log.info("Fin");
+        log.info(Messages.getString("App.9")); 
     }
 
 	
