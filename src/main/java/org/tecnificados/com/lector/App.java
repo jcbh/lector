1287bbeaaf2c21021ce5432b4fd094ea0426554d
Juan Carlos Ballesteros Hermida <jcballesteroshermida@gmail.com>
2019-10-06 17:02:38 +0200

    Fichero de prueba creado, y código para leer sus lineas

--- src/main/java/org/tecnificados/com/lector/App.java
+++ src/main/java/org/tecnificados/com/lector/App.java
@@ -1,13 +1,26 @@
 package org.tecnificados.com.lector;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
 /**
- * Hello world!
- *
+ * Lector de ficheros linea a linea
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	List<String> readedLines = new ArrayList<String>();
+        try {
+        	readedLines = FileUtils.readLines(new File("fichero.txt"),"utf-8");
+			System.out.println("Lineas: "+readedLines.size());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
