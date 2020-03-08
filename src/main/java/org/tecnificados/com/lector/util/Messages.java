e096a9487a4f2bdeb49c9cc33d06f45816f15ae3
Juan Carlos Ballesteros Hermida <jcballesteroshermida@gmail.com>
2020-03-08 14:50:11 +0100

    Maven changes

--- src/main/java/org/tecnificados/com/lector/util/Messages.java
+++ src/main/java/org/tecnificados/com/lector/util/Messages.java
@@ -5,7 +5,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "org.tecnificados.com.lector.util.messages"; 
+	private static final String BUNDLE_NAME = "messages"; 
 
 	private static ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
